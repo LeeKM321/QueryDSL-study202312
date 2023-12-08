3b4609c76c0aa151fd936b3bfddc27d1d22fd17a
kyoungmin <stephen4951@gmail.com>
2023-12-08 10:50:28 +0900

    #4. QueryDSL의 다양한 문법들(동적 SQL 활용법)

--- src/main/java/com/example/study/repository/MemberRepository.java
+++ src/main/java/com/example/study/repository/MemberRepository.java
@@ -2,6 +2,10 @@ package com.example.study.repository;
 
 import com.example.study.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long>,
         MemberRepositoryCustom {
